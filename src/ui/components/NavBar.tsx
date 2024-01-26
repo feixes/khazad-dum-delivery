570866930c2339fcf419c0e760212057e9a6f73c
Xavier Feixas <xavierfeixesguell@gmail.com>
2024-01-26 13:22:27 +0100

    navbar responsive for small screens

--- src/ui/components/NavBar.tsx
+++ src/ui/components/NavBar.tsx
@@ -1,19 +1,67 @@
-import { cross } from '../../assets/icons'
+import { useEffect, useRef, useState } from 'react'
+import { hamburgerMenu } from '../../assets/icons'
 
 export const NavBar = () => {
+
+    const hamMenu = useRef(null)
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleOpen = () => {
+        setIsOpen(prevState => !prevState)
+    }
+
+    useEffect(() => {
+
+        const closeMenu = (event) => {
+            if (!hamMenu.current.contains(event.target)) {
+                setIsOpen(false)
+            }
+        }
+
+        document.body.addEventListener('click', closeMenu)
+
+        return () => document.body.removeEventListener('click', closeMenu)
+    }, [])
+
+
+
+
     return (
-        <header className="p-4 border-4 flex text-slate-gray w-full">
-            <nav className="max-container flex flex-1 justify-between items-center">
+        <header className="text-slate-gray w-full">
+            <nav className="p-4 max-container flex flex-1 justify-between items-center">
                 <h4 className="text-5xl max-md:text-4xl font-bold font-palanquin text-[#505050] hover:text-black cursor-pointer">Anddara</h4>
-                <ul className="flex font-medium text-xl gap-10 font-montserrat max-lg:gap-4 max-lg:text-base max-md:hidden px-4">
+                <ul className="flex font-medium text-xl gap-10 font-montserrat max-lg:gap-4 max-lg:text-base max-md:hidden px-6">
                     <li className="hover:text-black cursor-pointer">Home</li>
                     <li className="hover:text-black cursor-pointer">¿Que hacemos?</li>
                     <li className="hover:text-black cursor-pointer">Sobre Nosotros</li>
                     <li className="hover:text-black cursor-pointer">Contáctanos</li>
                 </ul>
-                <p className="bg-primary text-[#505050] px-8 py-2 items-center font-montserrat font-bold rounded-full cursor-pointer max-sm:hidden">Acceder</p>
-                <img src={cross} height={40} width={40} alt="" />
+                <p className="bg-primary text-[#505050] px-8 py-2 items-center font-montserrat font-bold rounded-full cursor-pointer max-md:hidden">Acceder</p>
+                <img
+                    src={hamburgerMenu}
+                    height={40}
+                    width={40}
+                    alt="hamburger menu"
+                    className='cursor-pointer max-md:block hidden'
+                    ref={hamMenu}
+                    onClick={handleOpen}
+                />
             </nav>
+
+
+            {
+                isOpen && (
+                    <ul className="absolute w-full righ-[0%] border-2 flex flex-col pt-20 z-10 text-center font-medium text-2xl gap-10 font-montserrat px-6 border-b-2 pb-10 hidden max-md:flex">
+                        <li className="hover:text-black cursor-pointer">Home</li>
+                        <li className="hover:text-black cursor-pointer">¿Que hacemos?</li>
+                        <li className="hover:text-black cursor-pointer">Sobre Nosotros</li>
+                        <li className="hover:text-black cursor-pointer">Contáctanos</li>
+                        <li className="bg-primary text-[#505050] px-8 py-2 items-center font-montserrat font-bold rounded-full cursor-pointer">Acceder</li>
+                    </ul>
+                )
+            }
+
         </header>
     )
 }
