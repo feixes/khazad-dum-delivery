f48b5c87732e6af0c6ad2cbd4143c4091d55d919
Xavier Feixas <xavierfeixesguell@gmail.com>
2024-01-26 13:31:17 +0100

    navlinks and links for the router

--- src/ui/components/NavBar.tsx
+++ src/ui/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
 import { hamburgerMenu } from '../../assets/icons'
+import { Link, NavLink } from 'react-router-dom'
 
 export const NavBar = () => {
 
@@ -32,12 +33,22 @@ export const NavBar = () => {
             <nav className="p-4 max-container flex flex-1 justify-between items-center">
                 <h4 className="text-5xl max-md:text-4xl font-bold font-palanquin text-[#505050] hover:text-black cursor-pointer">Anddara</h4>
                 <ul className="flex font-medium text-xl gap-10 font-montserrat max-lg:gap-4 max-lg:text-base max-md:hidden px-6">
-                    <li className="hover:text-black cursor-pointer">Home</li>
-                    <li className="hover:text-black cursor-pointer">¿Que hacemos?</li>
-                    <li className="hover:text-black cursor-pointer">Sobre Nosotros</li>
-                    <li className="hover:text-black cursor-pointer">Contáctanos</li>
+                    <NavLink to='/'>
+                        <li className="hover:text-black cursor-pointer">Home</li>
+                    </NavLink>
+                    <NavLink to='details'>
+                        <li className="hover:text-black cursor-pointer">¿Que hacemos?</li>
+                    </NavLink>
+                    <NavLink to='about'>
+                        <li className="hover:text-black cursor-pointer">Sobre Nosotros</li>
+                    </NavLink>
+                    <NavLink to='contact'>
+                        <li className="hover:text-black cursor-pointer">Contáctanos</li>
+                    </NavLink>
                 </ul>
-                <p className="bg-primary text-[#505050] px-8 py-2 items-center font-montserrat font-bold rounded-full cursor-pointer max-md:hidden">Acceder</p>
+                <Link to='auth/login'>
+                    <p className="bg-primary text-[#505050] px-8 py-2 items-center font-montserrat font-bold rounded-full cursor-pointer max-md:hidden">Acceder</p>
+                </Link>
                 <img
                     src={hamburgerMenu}
                     height={40}
@@ -52,12 +63,22 @@ export const NavBar = () => {
 
             {
                 isOpen && (
-                    <ul className="absolute w-full righ-[0%] border-2 flex flex-col pt-20 z-10 text-center font-medium text-2xl gap-10 font-montserrat px-6 border-b-2 pb-10 hidden max-md:flex">
-                        <li className="hover:text-black cursor-pointer">Home</li>
-                        <li className="hover:text-black cursor-pointer">¿Que hacemos?</li>
-                        <li className="hover:text-black cursor-pointer">Sobre Nosotros</li>
-                        <li className="hover:text-black cursor-pointer">Contáctanos</li>
-                        <li className="bg-primary text-[#505050] px-8 py-2 items-center font-montserrat font-bold rounded-full cursor-pointer">Acceder</li>
+                    <ul className="absolute w-full righ-[0%] border-2  flex-col pt-20 z-10 text-center font-medium text-2xl gap-10 font-montserrat px-6 border-b-2 pb-10 hidden max-md:flex">
+                        <Link to='/'>
+                            <li className="hover:text-black cursor-pointer">Home</li>
+                        </Link>
+                        <Link to='details'>
+                            <li className="hover:text-black cursor-pointer">¿Que hacemos?</li>
+                        </Link>
+                        <Link to='about'>
+                            <li className="hover:text-black cursor-pointer">Sobre Nosotros</li>
+                        </Link>
+                        <Link to='contact'>
+                            <li className="hover:text-black cursor-pointer">Contáctanos</li>
+                        </Link>
+                        <Link to='auth/login'>
+                            <li className="bg-primary text-[#505050] px-8 py-2 items-center font-montserrat font-bold rounded-full cursor-pointer">Acceder</li>
+                        </Link>
                     </ul>
                 )
             }
