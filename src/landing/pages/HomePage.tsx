c1b799bf66300e81b7677cc9e37768240286dd95
Xavier Feixas <xavierfeixesguell@gmail.com>
2024-02-04 21:45:09 +0100

    features section responsive

--- src/landing/pages/HomePage.tsx
+++ src/landing/pages/HomePage.tsx
@@ -8,13 +8,12 @@ import { featuresConstants } from "../constants/FeaturesConstants"
 export const HomePage = () => {
     return (
         <>
-            <section className="mt-10 min-h-[90vh]">
-
+            <section className="flex justify-center items-center min-h-[90vh]">
                 <Hero />
             </section>
 
-            <section className=" padding bg-primary w-full">
-                <div className="max-container bg-inherit padding-x flex justify-around">
+            <section className="padding bg-primary w-full">
+                <div className="max-container bg-inherit padding-x grid grid-cols-3 max-xl:grid-cols-2 max-lg:grid-cols-1 place-items-center gap-10">
 
                     {
                         featuresConstants.map(featureItem => (
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ export default {
         "black-pale": "#505050",
         "coral-red": "#FF6452",
         "slate-gray": "#6D6D6D",
-        "pale-blue": "#F5F6FF",
+        "pale-blue": "#fafffe",
         "white-400": "rgba(255, 255, 255, 0.80)"
       },
       fontSize: {
