072d8c69efe7a10d26ae6ab605c881884dae7cfe
Xavier Feixas <xavierfeixesguell@gmail.com>
2024-02-08 20:44:39 +0100

    refactor on hero

--- src/landing/components/Hero.tsx
+++ src/landing/components/Hero.tsx
@@ -13,9 +13,13 @@ export const Hero = () => {
                 </h1>
 
                 <p className="text-black-pale mt-6 text-2xl leading-10">Transportamos cualquier tipo de joyería para su posterior destrucción en la fragua que lo forjó</p>
-                <button className="group flex justify-center btn-primary w-full py-4 text-2xl mt-20">
+
+                <button className="group flex justify-center btn-primary w-full py-4 text-2xl mt-20 text-black-pale transition duration-500">
                     Acceder
-                    <img src={rightArrow} alt={rightArrow} className="ml-2 rounded-full w-5 h-5 bg-primary group-hover:bg-primary-dark group-hover:translate-x-5 transition duration-500" />
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill='none' viewBox="0 0 24 24" stroke-width="3" stroke='currentColor' className="ml-2 rounded-full w-5 h-5 bg-primary group-hover:bg-primary-dark group-hover:translate-x-5 transition duration-500 text-black-pale group-hover:text-black">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                    </svg>
                 </button>
             </div>
 
