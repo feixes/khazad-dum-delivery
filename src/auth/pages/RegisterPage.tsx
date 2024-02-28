6fc404ccb5033379ca95f7c0a362532857c04a74
Xavier Feixas <88280443+feixes@users.noreply.github.com>
2024-02-28 03:27:10 +0100

    fixed size

--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -1,13 +1,13 @@
 import { key, mail, user } from "../../assets/icons"
-import { login } from "../../assets/images"
+// import { login } from "../../assets/images"
 import { LoginInput } from "../components/LoginInput"
 
 
 
 export const RegisterPage = () => {
     return (
-        <main className="w-full max-h-[95vh] padding flex">
-            <section className="flex max-container gap-10 text-2xl text-black-pale shadow-3xl">
+        <main className="w-full min-h-[80vh] m-10 flex">
+            <section className="flex max-container gap-10 text-lg text-black-pale shadow-3xl w-full">
                 <div className="flex flex-col flex-1 m-10">
                     <div>
                         <h1 className="text-4xl font-palaquin font-semibold tracking-tighter">Regístrate!</h1>
@@ -22,12 +22,14 @@ export const RegisterPage = () => {
                         <LoginInput id="password" type="password" label="Contraseña" icon={key} />
                         <LoginInput id="password-repeat" type="password" label="Repite la Contraseña" icon={key} />
 
-                        <button className="btn-primary py-4 mt-20">Login</button>
+                        <button className="btn-primary py-4 mt-10">Login</button>
 
                     </form>
                 </div>
 
-                <img className="flex-1 w-[50%]" src={login} alt="" />
+                <div className="flex-1 bg-login">
+                    {/* <img className="w-full h-full" src={login} alt="a hobbit hole" /> */}
+                </div>
             </section>
         </main >
     )
